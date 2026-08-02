d51bb657b2b7121de5edb410c0c0eace1c769908
Rahul Ravikumar <tikurahul@gmail.com>
2026-08-02 02:01:48 -0700

    Centered positioning

--- desktopApp/src/main/kotlin/com/rahulrav/main.kt
+++ desktopApp/src/main/kotlin/com/rahulrav/main.kt
@@ -1,19 +1,22 @@
 package com.rahulrav
 
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     val windowState = rememberWindowState(
         width = 3600.dp,
-        height = 2400.dp
+        height = 2400.dp,
+        position = WindowPosition.Aligned(alignment = Alignment.Center)
     )
     Window(
         onCloseRequest = ::exitApplication,
         state = windowState,
-        title = "Warp",
+        title = "Warp ✨",
     ) {
         Warp()
     }
